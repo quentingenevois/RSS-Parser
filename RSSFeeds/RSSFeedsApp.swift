260f626a7be95d1c7605d6154766e800efbcce65
Quentin Genevois <quentin.g@appsolute.fr>
2023-12-29 21:39:43 +0100

    Initial Commit

---
+++ RSSFeeds/RSSFeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSFeedsApp.swift
+//  RSSFeeds
+//
+//  Created by Quentin Genevois on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RSSFeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
